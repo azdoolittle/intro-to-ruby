6f05813447841ef9cf991a33dea596fa69aa2849
Andrew Doolittle <macbook@711-Kwik-E-Mart.local>
2019-03-21 17:38:03 -0400

    Code modified after viewing solution

--- intro-to-ruby/1-basics/e3.rb
+++ intro-to-ruby/1-basics/e3.rb
@@ -1,3 +1,8 @@
-movies = {"Title A" => 1975, "Title B" => 1981, "Title C" => 2001, "Title D" => 2004, "Title E" => 2013}
+movies = { 
+	"Title_A" => 1975, 
+	"Title_B" => 1981, 
+	"Title_C" => 2001, 
+	"Title_D" => 2004, 
+	"Title_E" => 2013 }
 
-movies.each { |title, year| puts "#{title}: #{year}" }
+movies.each { |title, year| puts "#{year}" }
