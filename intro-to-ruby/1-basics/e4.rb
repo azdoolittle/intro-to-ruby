beaec0d72b4d691865d18d8207c6cfcea04e22e8
Andrew Doolittle <macbook@711-Kwik-E-Mart.local>
2019-03-21 17:56:31 -0400

    Commit 4 e4.rb

---
+++ intro-to-ruby/1-basics/e4.rb
@@ -0,0 +1,6 @@
+# Use the dates from the previous example and store them in an array. 
+# Then make your program output the same thing as exercise 3.
+
+year = [1975, 1981, 2001, 2004, 2013]
+
+year.each { | year| puts "#{year}" }
