74ab01a8359d4d53d13e5cbaedc22c85182402aa
Andrew Doolittle <macbook@711-Kwik-E-Mart.local>
2019-04-10 14:27:55 -0400

    adding e6.rb

---
+++ 1-basics/e6.rb
@@ -0,0 +1,10 @@
+# Write a program that calculates 
+# the squares of 3 float numbers of your choosing 
+# and outputs the result to the screen.
+
+def square(num)
+	product = num * num
+	puts product
+end
+
+puts "Calculate the square of a float number by entering 'square(x)', where x is a float number."
