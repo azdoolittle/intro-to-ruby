18d1df4a7a402e86fff15f0eeabd72c11e497517
Andrew Doolittle <macbook@711-Kwik-E-Mart.local>
2019-04-18 16:48:36 -0400

    modified e5.rb with correct solution

--- 2-variables/e5.rb
+++ 2-variables/e5.rb
@@ -22,9 +22,15 @@
 # 
 # 1) In the first example, x prints out 1, 2, 3 respectively.
 #    In the second example, an error occurs.
+#    
+#    According to the solution: x prints out 3. 
+#    This is because x is reassigned to 1, then 2, then 3.
 #   
 # 2) Only example 2 gives an error.
 # 
 # 3) In the second example, an error occurs because x is not available 
 #    outside of the method invocation with a block where it is initialized. 
 #    It's not available within that outer scope.
+#    
+# 4) According to the solution: y was initialized in the outer scope and 
+#    therefore accessible in the inner scope.
